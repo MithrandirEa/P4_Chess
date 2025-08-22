59916420d628d3a0747c9ec9d91ab9d0e3386b4a
Clément SCIPION <clscipion@gmail.com>
2025-08-22 22:53:40 +0200

    Création des fichier du controler. Découpé en deux fichiers, report_control pour les rapports et tournaments_control pour la gestion des logique métier du tournois.

---
+++ Controler/report_control.py
@@ -0,0 +1,7 @@
+""" report_control.py - Contrôleur des rapports."""
+
+from __future__ import annotations
+from Models.tournament import Tournament
+from Models.round import Round
+from Models.match import Match
+from Models.player import Player
---
+++ Controler/tournaments_control.py
@@ -0,0 +1,8 @@
+""" tournaments_control.py - Contrôleur des tournois."""
+
+from __future__ import annotations
+from Models.tournament import Tournament
+from Models.round import Round
+from Models.match import Match
+from Models.player import Player
+
